8dc0363de68e5dfcb78e051333a21c96f2477dd5
renatabeica20 <renata.beica20@gmail.com>
2026-04-28 11:28:10 -0400

    Add files via upload

--- api/get-list.js
+++ api/get-list.js
@@ -1,1 +1,17 @@
+export default async function handler(req, res) {
+  const { id } = req.query;
 
+  const response = await fetch(
+    `${process.env.VITE_SUPABASE_URL}/rest/v1/shared_lists?id=eq.${id}&select=*`,
+    {
+      headers: {
+        apikey: process.env.VITE_SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${process.env.VITE_SUPABASE_ANON_KEY}`
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data[0] || {});
+}
