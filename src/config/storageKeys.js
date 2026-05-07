bf935646472a0eb3e55a8a7c3afe93fab44856b6
renatabeica20 <renata.beica20@gmail.com>
2026-05-07 08:25:50 -0400

    Update storageKeys.js

--- src/config/storageKeys.js
+++ src/config/storageKeys.js
@@ -1,1 +1,19 @@
-teste
+export const APP_USER_NAME_KEY = "tnl_user_name";
+export const APP_DEVICE_ID_KEY = "tnl_device_id";
+export const APP_USER_REGISTERED_KEY = "tnl_user_registered_device_id";
+export const APP_USER_ID_KEY = "tnl_user_id";
+
+export const APP_PIN_SESSION_NAME_KEY = "tnl_pin_verified_name";
+export const APP_PIN_SESSION_AT_KEY = "tnl_pin_verified_at";
+
+export const APP_INSTALL_PROMPT_DISMISSED_KEY =
+  "tnl_install_prompt_dismissed";
+
+export const APP_INSTALL_PROMPT_LAST_SHOWN_KEY =
+  "tnl_install_prompt_last_shown";
+
+export const APP_GUIDED_TOUR_DONE_KEY =
+  "tnl_guided_tour_done_v2";
+
+export const APP_GUIDED_TOUR_DISMISSED_KEY =
+  "tnl_guided_tour_dismissed_v2";
